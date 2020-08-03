2591e6de8388e48c4d31c3cbbf587d40d15eb3f2
tadanotomo <to.iijima@gmail.com>
2020-08-03 16:25:14 +0900

    入力ミス変更

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,11 @@ module ChatApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config geerators do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helpers false
       g.test_framework false
+    end
   end
 end
