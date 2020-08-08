cfcd50bd75c9916b0305642be31be7fa0cbc3bb6
tadanotomo <to.iijima@gmail.com>
2020-08-08 15:04:46 +0900

    messagesテーブルの作成

--- db/migrate/20200808055515_create_messages.rb
+++ db/migrate/20200808055515_create_messages.rb
@@ -1,6 +1,7 @@
-class CreateRoomUsers < ActiveRecord::Migration[6.0]
+class CreateMessages < ActiveRecord::Migration[6.0]
   def change
-    create_table :room_users do |t|
+    create_table :messages do |t|
+      t.string :content
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
