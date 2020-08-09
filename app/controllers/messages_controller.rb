10d54e03fe44c2aa00764701db09ce5a3379b52b
tadanotomo <to.iijima@gmail.com>
2020-08-09 10:45:49 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,7 +11,7 @@ class MessagesController < ApplicationController
     @message = @room.messages.new(message_params)
     @message.save
 
-    if @message.save = true
+    if @message.save
       redirect_to room_messages_path(@room)
     else
       render :index
